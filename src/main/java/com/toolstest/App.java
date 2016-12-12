eaadd0fc9bd24f9c51333382b2f938897433c284
Stephen Hahn <stevetx30@gmail.com>
2016-12-12 14:56:30 -0600

    added in new PMD error

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -10,4 +10,14 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    public void bar(int x, int y, int z) {
+        if (x>y) {
+            if (y>z) {
+                if (z==x) {
+                    // whew, too deep
+                }
+            }
+        }
+    }
 }
